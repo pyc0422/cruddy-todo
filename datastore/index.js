29dde4ffe8e59a279294e9655bbfe8ddfc5f6d30
Daisy Ding <xd222@cornell.edu>
2022-08-04 18:18:16 -0700

    Complete deleting a todo

--- datastore/index.js
+++ datastore/index.js
@@ -82,14 +82,22 @@ exports.update = (id, text, callback) => {
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  // var item = items[id];
+  // delete items[id];
+  // if (!item) {
+  //   // report an error if item not found
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   callback();
+  // }
+  var file = path.join(exports.dataDir, id + '.txt');
+  fs.rm(file, function(err) { //fs.unlink works too
+    if (err) {
+      callback(err);
+    } else {
+      callback();
+    }
+  });
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
