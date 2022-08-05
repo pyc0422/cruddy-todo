e23c760c1011f6afff693fdc7487e7180047f9fd
Daisy Ding <xd222@cornell.edu>
2022-08-04 19:55:38 -0700

    Complete BMR V2

--- datastore/index.js
+++ datastore/index.js
@@ -26,16 +26,37 @@ exports.create = (text, callback) => {
 };
 
 exports.readAll = (callback) => {
+  // const readFiles = (dirname) => {
+  //   const readDirPr = new Promise( (resolve, reject) => {
+  //     fs.readdir(dirname,
+  //       (err, filenames) => (err) ? reject(err) : resolve(filenames));
+  //   });
 
-  const readFiles = (dirname) => {
-    const readDirPr = new Promise( (resolve, reject) => {
-      fs.readdir(dirname,
-        (err, filenames) => (err) ? reject(err) : resolve(filenames));
-    });
+  // return readDirPr.then( filenames => Promise.all(filenames.map((filename) => {
+  //   return new Promise ( (resolve, reject) => {
+  //     fs.readFile(dirname + '/' + filename, 'utf-8', function(err, content) {
+  //       if (err) {
+  //         reject(err);
+  //       } else {
+  //         var id = filename.split('.')[0];
+  //         resolve({id, text: content});
+  //       }
+  //     });
+  //   });
+  //   })).catch( error => Promise.reject(error)));
+  // };
 
-    return readDirPr.then( filenames => Promise.all(filenames.map((filename) => {
+  // readFiles(exports.dataDir)
+  //   .then( allContents => {
+  //     console.log('allcontents: ', allContents);
+  //     callback(null, allContents);
+  //   }, error => console.log(error));
+  return new Promise((resolve, reject) => {
+    fs.readdir(exports.dataDir, (err, filenames) => (err) ? reject(err) : resolve(filenames));
+  })
+    .then( filenames => Promise.all(filenames.map((filename) => {
       return new Promise ( (resolve, reject) => {
-        fs.readFile(dirname + '/' + filename, 'utf-8', function(err, content) {
+        fs.readFile(exports.dataDir + '/' + filename, 'utf-8', function(err, content) {
           if (err) {
             reject(err);
           } else {
@@ -44,51 +65,12 @@ exports.readAll = (callback) => {
           }
         });
       });
-    })).catch( error => Promise.reject(error)));
-  };
-
-  readFiles(exports.dataDir)
-    .then( allContents => {
-      console.log('allcontents: ', allContents);
-      callback(null, allContents);
-    }, error => console.log(error));
-  // var promise = fs.readdir(exports.dataDir, function(err, items) {
-  //   if (err) {
-  //     callback(err);
-  //   } else {
-  //     // var data = _.map(items, (text) => {
-  //     //   var id = text.split('.')[0];
-  //     //   return {id, text: id};
-  //     // });
-
-  //     return items.forEach((text) => {
-  //       fs.readFile(exports.dataDir + '/' + text, 'utf8', function(err, content) {
-  //         console.log({text: content});
-  //         if (err) {
-  //           callback(err);
-  //         } else {
-  //           var id = text.split('.')[0];
-  //           console.log('before return: ', {id, text: content});
-  //           callback(null, {id, text: content});
-  //         }
-  //       }).then((content) => {data.push(content);});
-  //     });
-  //     Promise.all(promise).then((values)=> {
-  //       console.log('values: ', values);
-  //       callback(null, values);
-  //     }).catch((err) => {console.log(err);})
-
-  //   }
-  // });
+    })).catch( error => Promise.reject(error)))
+    .then((values) => { callback(null, values); });
 };
 
 exports.readOne = (id, callback) => {
-  // var text = items[id];
-  // if (!text) {
-  //   callback(new Error(`No item with id: ${id}`));
-  // } else {
-  //   callback(null, { id, text });
-  // }
+
   var file = path.join(exports.dataDir, id + '.txt');
   fs.readFile(file, 'utf8', function(err, content) {
     if (err) {
@@ -100,13 +82,7 @@ exports.readOne = (id, callback) => {
 };
 
 exports.update = (id, text, callback) => {
-  // var item = items[id];
-  // if (!item) {
-  //   callback(new Error(`No item with id: ${id}`));
-  // } else {
-  //   items[id] = text;
-  //   callback(null, { id, text });
-  // }
+
   var file = path.join(exports.dataDir, id + '.txt');
   exports.readOne(id, function(err, content) {
     if (err) {
