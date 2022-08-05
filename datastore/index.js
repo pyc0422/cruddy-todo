3db1b76fed24fc88ec9190a245894a7e02174056
Daisy Ding <xd222@cornell.edu>
2022-08-04 19:38:18 -0700

    complete BMR

--- datastore/index.js
+++ datastore/index.js
@@ -26,17 +26,60 @@ exports.create = (text, callback) => {
 };
 
 exports.readAll = (callback) => {
-  fs.readdir(exports.dataDir, function(err, items) {
-    if (err) {
-      callback(err);
-    } else {
-      var data = _.map(items, (text) => {
-        var id = text.split('.')[0];
-        return {id, text: id};
+
+  const readFiles = (dirname) => {
+    const readDirPr = new Promise( (resolve, reject) => {
+      fs.readdir(dirname,
+        (err, filenames) => (err) ? reject(err) : resolve(filenames));
+    });
+
+    return readDirPr.then( filenames => Promise.all(filenames.map((filename) => {
+      return new Promise ( (resolve, reject) => {
+        fs.readFile(dirname + '/' + filename, 'utf-8', function(err, content) {
+          if (err) {
+            reject(err);
+          } else {
+            var id = filename.split('.')[0];
+            resolve({id, text: content});
+          }
+        });
       });
-      callback(null, data);
-    }
-  });
+    })).catch( error => Promise.reject(error)));
+  };
+
+  readFiles(exports.dataDir)
+    .then( allContents => {
+      console.log('allcontents: ', allContents);
+      callback(null, allContents);
+    }, error => console.log(error));
+  // var promise = fs.readdir(exports.dataDir, function(err, items) {
+  //   if (err) {
+  //     callback(err);
+  //   } else {
+  //     // var data = _.map(items, (text) => {
+  //     //   var id = text.split('.')[0];
+  //     //   return {id, text: id};
+  //     // });
+
+  //     return items.forEach((text) => {
+  //       fs.readFile(exports.dataDir + '/' + text, 'utf8', function(err, content) {
+  //         console.log({text: content});
+  //         if (err) {
+  //           callback(err);
+  //         } else {
+  //           var id = text.split('.')[0];
+  //           console.log('before return: ', {id, text: content});
+  //           callback(null, {id, text: content});
+  //         }
+  //       }).then((content) => {data.push(content);});
+  //     });
+  //     Promise.all(promise).then((values)=> {
+  //       console.log('values: ', values);
+  //       callback(null, values);
+  //     }).catch((err) => {console.log(err);})
+
+  //   }
+  // });
 };
 
 exports.readOne = (id, callback) => {
